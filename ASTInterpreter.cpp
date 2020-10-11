29d88dd4bb12ae824de22a0a959b19eaf2b881e5
imhcyx <3443722+imhcyx@users.noreply.github.com>
2020-10-11 09:18:18 +0800

    Refactor & implement if statement

--- ASTInterpreter.cpp
+++ ASTInterpreter.cpp
@@ -12,70 +12,174 @@ using namespace clang;
 #include "Environment.h"
 
 class InterpreterVisitor : 
-   public EvaluatedExprVisitor<InterpreterVisitor> {
+    public EvaluatedExprVisitor<InterpreterVisitor> {
 public:
-   explicit InterpreterVisitor(const ASTContext &context, Environment * env)
-   : EvaluatedExprVisitor(context), mEnv(env) {}
-   virtual ~InterpreterVisitor() {}
+    explicit InterpreterVisitor(const ASTContext &context)
+    : EvaluatedExprVisitor(context), mStack(), mFree(NULL), mMalloc(NULL), mInput(NULL), mOutput(NULL), mEntry(NULL) {}
+    virtual ~InterpreterVisitor() {}
 
-   virtual void VisitUnaryOperator (UnaryOperator * uop) {
-	   VisitStmt(uop);
-      mEnv->unop(uop);
-   }
-   virtual void VisitBinaryOperator (BinaryOperator * bop) {
-      //llvm::errs() << "binop: " << bop->getOpcodeStr() << "\n";
-	   VisitStmt(bop);
-	   mEnv->binop(bop);
-   }
-   virtual void VisitDeclRefExpr(DeclRefExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->declref(expr);
-   }
-   virtual void VisitCastExpr(CastExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->cast(expr);
-   }
-   virtual void VisitCallExpr(CallExpr * call) {
-	   VisitStmt(call);
-	   mEnv->call(call);
-   }
-   virtual void VisitDeclStmt(DeclStmt * declstmt) {
-	   mEnv->decl(declstmt);
-   }
+    void init(TranslationUnitDecl * unit) {
+        for (TranslationUnitDecl::decl_iterator i =unit->decls_begin(), e = unit->decls_end(); i != e; ++ i) {
+            if (FunctionDecl * fdecl = dyn_cast<FunctionDecl>(*i) ) {
+                if (fdecl->getName().equals("FREE")) mFree = fdecl;
+                else if (fdecl->getName().equals("MALLOC")) mMalloc = fdecl;
+                else if (fdecl->getName().equals("GET")) mInput = fdecl;
+                else if (fdecl->getName().equals("PRINT")) mOutput = fdecl;
+                else if (fdecl->getName().equals("main")) mEntry = fdecl;
+            }
+        }
+        mStack.push_back(StackFrame());
+    }
+
+    void start() {
+        VisitStmt(mEntry->getBody());
+    }
+
+    virtual void VisitUnaryOperator(UnaryOperator * uop) {
+        VisitStmt(uop);
+
+        Expr *sub = uop->getSubExpr();
+        int val = mStack.back().getStmtVal(sub);
+        int result = 0;
+
+        switch (uop->getOpcode()) {
+            case UO_Plus:   result = val;       break;
+            case UO_Minus:  result = -val;      break;
+        }
+        mStack.back().bindStmt(uop, result);
+    }
+
+    virtual void VisitBinaryOperator(BinaryOperator * bop) {
+        VisitStmt(bop);
+
+        Expr * left = bop->getLHS();
+        Expr * right = bop->getRHS();
+
+        if (bop->isAssignmentOp()) {
+            int val = mStack.back().getStmtVal(right);
+            if (DeclRefExpr * declexpr = dyn_cast<DeclRefExpr>(left)) {
+                Decl * decl = declexpr->getFoundDecl();
+                mStack.back().bindDecl(decl, val);
+            }
+        }
+        else {
+            int lhs = mStack.back().getStmtVal(left);
+            int rhs = mStack.back().getStmtVal(right);
+            int result = 0;
+            switch (bop->getOpcode()) {
+                case BO_Add:    result = lhs + rhs;     break;
+                case BO_Sub:    result = lhs - rhs;     break;
+                case BO_Mul:    result = lhs * rhs;     break;
+                case BO_Div:    result = lhs / rhs;     break;
+                case BO_GT:     result = lhs > rhs;     break;
+                case BO_LT:     result = lhs < rhs;     break;
+                case BO_EQ:     result = lhs == rhs;    break;
+            }
+            mStack.back().bindStmt(bop, result);
+            //llvm::errs() << "binop " << bop->getOpcodeStr() << ": " << result << "\n";
+        }
+    }
+
+    virtual void VisitDeclRefExpr(DeclRefExpr * expr) {
+        VisitStmt(expr);
+        mStack.back().setPC(expr);
+        if (expr->getType()->isIntegerType()) {
+            Decl* decl = expr->getFoundDecl();
+
+            int val = mStack.back().getDeclVal(decl);
+            mStack.back().bindStmt(expr, val);
+        }
+    }
+
+    virtual void VisitCastExpr(CastExpr * expr) {
+        VisitStmt(expr);
+        mStack.back().setPC(expr);
+        if (expr->getType()->isIntegerType()) {
+            Expr * sub = expr->getSubExpr();
+            int val = mStack.back().getStmtVal(sub);
+            mStack.back().bindStmt(expr, val);
+        }
+    }
+
+    virtual void VisitCallExpr(CallExpr * call) {
+        VisitStmt(call);
+        mStack.back().setPC(call);
+        int val = 0;
+        FunctionDecl * callee = call->getDirectCallee();
+        if (callee == mInput) {
+           llvm::errs() << "Please Input an Integer Value : ";
+           scanf("%d", &val);
+
+           mStack.back().bindStmt(call, val);
+        } else if (callee == mOutput) {
+            Expr * decl = call->getArg(0);
+            val = mStack.back().getStmtVal(decl);
+            llvm::errs() << val;
+        } else {
+            /// You could add your code here for Function call Return
+        }
+    }
+
+    virtual void VisitIfStmt(IfStmt *stmt) {
+        Expr *cond = stmt->getCond();
+        Visit(cond);
+
+        int val = mStack.back().getStmtVal(cond);
+        if (val) {
+            VisitStmt(stmt->getThen());
+        }
+        else {
+            VisitStmt(stmt->getElse());
+        }
+    }
+
+    virtual void VisitDeclStmt(DeclStmt * declstmt) {
+        for (DeclStmt::decl_iterator it = declstmt->decl_begin(), ie = declstmt->decl_end();
+                it != ie; ++ it) {
+            Decl * decl = *it;
+            if (VarDecl * vardecl = dyn_cast<VarDecl>(decl)) {
+                mStack.back().bindDecl(vardecl, 0);
+            }
+        }
+    }
+
 private:
-   Environment * mEnv;
+    std::vector<StackFrame> mStack;
+
+    FunctionDecl * mFree;				/// Declartions to the built-in functions
+    FunctionDecl * mMalloc;
+    FunctionDecl * mInput;
+    FunctionDecl * mOutput;
+
+    FunctionDecl * mEntry;
 };
 
 class InterpreterConsumer : public ASTConsumer {
 public:
-   explicit InterpreterConsumer(const ASTContext& context) : mEnv(),
-   	   mVisitor(context, &mEnv) {
-   }
-   virtual ~InterpreterConsumer() {}
+    explicit InterpreterConsumer(const ASTContext& context) : mVisitor(context) {}
+    virtual ~InterpreterConsumer() {}
 
-   virtual void HandleTranslationUnit(clang::ASTContext &Context) {
-	   TranslationUnitDecl * decl = Context.getTranslationUnitDecl();
-	   mEnv.init(decl);
+    virtual void HandleTranslationUnit(clang::ASTContext &Context) {
+        TranslationUnitDecl * decl = Context.getTranslationUnitDecl();
 
-	   FunctionDecl * entry = mEnv.getEntry();
-	   mVisitor.VisitStmt(entry->getBody());
+        mVisitor.init(decl);
+        mVisitor.start();
   }
 private:
-   Environment mEnv;
-   InterpreterVisitor mVisitor;
+    InterpreterVisitor mVisitor;
 };
 
 class InterpreterClassAction : public ASTFrontendAction {
 public: 
   virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-    clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
-    return std::unique_ptr<clang::ASTConsumer>(
-        new InterpreterConsumer(Compiler.getASTContext()));
+     clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
+     return std::unique_ptr<clang::ASTConsumer>(
+          new InterpreterConsumer(Compiler.getASTContext()));
   }
 };
 
 int main (int argc, char ** argv) {
-   if (argc > 1) {
-       clang::tooling::runToolOnCode(std::unique_ptr<clang::FrontendAction>(new InterpreterClassAction), argv[1]);
-   }
+    if (argc > 1) {
+         clang::tooling::runToolOnCode(std::unique_ptr<clang::FrontendAction>(new InterpreterClassAction), argv[1]);
+    }
 }
